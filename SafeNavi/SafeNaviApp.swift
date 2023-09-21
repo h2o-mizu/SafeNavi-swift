3e5c8812ca10c37a9ce52d930e3e67dc67fead80
Alicelavander <alicelavander.dev@gmail.com>
2023-09-21 11:11:49 -0400

    Initial Commit

---
+++ SafeNavi/SafeNaviApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeNaviApp.swift
+//  SafeNavi
+//
+//  Created by Arisa Okamura on 2023/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SafeNaviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
